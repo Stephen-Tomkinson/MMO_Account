4ea682253fc980ef30cb774888e43eb3531d22ab
Stephen-Tomkinson <stephenisatomkinson@gmail.com>
2022-02-07 14:53:55 +0000

    serviceinterface developed

--- src/main/java/com/qa/mmoaccount/service/ServiceInterface.java
+++ src/main/java/com/qa/mmoaccount/service/ServiceInterface.java
@@ -1,13 +1,21 @@
 package com.qa.mmoaccount.service;
 
+import java.util.List;
+
 public interface ServiceInterface<T> {
 
 	// Create
+	T create(T t);
 
 	// Read [All - ID]
+	List<T> readAll(); // All
+
+	T readById(Long id);
 
 	// Update
+	T update(Long id, T t);
 
 	// Delete
+	T delete(Long id);
 
 }

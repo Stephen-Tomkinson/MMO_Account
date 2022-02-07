549ebb5a05ed1929149645de5d1b7226f4e54eb0
Stephen-Tomkinson <stephenisatomkinson@gmail.com>
2022-02-07 11:38:01 +0000

    set up AccountRepo

--- src/main/java/com/qa/mmoaccount/repo/AccountRepo.java
+++ src/main/java/com/qa/mmoaccount/repo/AccountRepo.java
@@ -1,5 +1,11 @@
 package com.qa.mmoaccount.repo;
 
-public class AccountRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.mmoaccount.domain.Account;
+
+public interface AccountRepo extends JpaRepository<Account, Long> {
+
+	// Retrieve from db
 
 }

9a9de39650e5a99685c0a5afe0e686dbb59b68dc
Stephen-Tomkinson <stephenisatomkinson@gmail.com>
2022-02-07 11:39:57 +0000

    set up Account - Domain

--- src/main/java/com/qa/mmoaccount/domain/Account.java
+++ src/main/java/com/qa/mmoaccount/domain/Account.java
@@ -2,4 +2,19 @@ package com.qa.mmoaccount.domain;
 
 public class Account {
 
+	// PK
+
+	// Name
+
+	// Level
+
+	// Constructors[No Args, All Args[No ID], All Args
+
+	// Getters & Setters
+
+	// HashCode
+
+	// Equals
+
+	// ToString
 }
